21dbb041af3de3dcfbe558d60b55cfc3a49ef744
jb-rough <jb_rough@hotmail.com>
2014-09-21 16:00:35 -0500

    Update cachematrix.R
    Final version for grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Based on the outline from the vector examples
+#Given the makeCacheMatrix function, assume x is an invertable matrix
+#note!! this only works if you assign makeCacheVector to a variable and then
+#run cacheSolve on that variable, otherwise you reinitiaize m every time
+#which is probably obvious, but took me several hours to figure out
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+#initialize m and create set function y with x (matix) and m (inverse matrix)
+  m <- (NULL)
+  y <- (NULL) 
+  set <- function(y){
+  x <<- (y)
+  m <<- (NULL)
 }
 
+#create get function 
+get <- function() x
 
-## Write a short comment describing this function
+#create setinvmatrix function to solve for inverse of matrix
+setinvmatrix <- function(solve) m <<- solve
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#create getinvmatrix function to return cached matrix 
+getinvmatrix <- function() m
+
+#set up list with set,get, setinvmatrix and getinvmatrix functions
+list(set=set, get=get, setinvmatrix=setinvmatrix, getinvmatrix=getinvmatrix)
+
+}
+
+#given cacheSolve function with arguments passed from makeCacheMatrix
+cacheSolve <- function(x = matrix(), ...) {
+
+#if m exists then return m as the inverse of the matrix 
+    m <- x$getinvmatrix() 
+    print(m)
+    if(!is.null(m)){
+      message("Returning cached data...")
+      return(m)
+    }
+    
+#if m is null then calculate the inverse of the matrix and cache the value
+    data <- x$get()
+    m <- solve(data)
+    x$setinvmatrix(m)
+    m
+
 }
